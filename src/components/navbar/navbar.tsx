7ca9fe40489077be29c7a3658745fb84b14fb17d
fahmi-mac <f.hidayah@pixilapps.com>
2025-08-07 17:40:43 +0700

    fix auth

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,20 +1,18 @@
 'use client';
-import Link from 'next/link';
-import { useSession } from 'next-auth/react';
+// import Link from 'next/link';
+// import { useSession } from 'next-auth/react';
 
-import { Button } from '../ui/button';
+// import { Button } from '../ui/button';
 
-import { SignInButton } from '@/components/navbar/sign-in-button';
-import { UserDropdown } from '@/components/navbar/user-dropdown';
+// import { SignInButton } from '@/components/navbar/sign-in-button';
+// import { UserDropdown } from '@/components/navbar/user-dropdown';
 
 export const Navbar = () => {
-  const session = useSession();
-
   return (
     <header className="container mx-auto flex items-center  justify-between py-6">
       <h1 className="text-primary text-2xl font-bold">QuestionApp</h1>
       <nav className="space-x-4">
-        {session.status === 'authenticated' ? (
+        {/* {session.status === 'authenticated' ? (
           <UserDropdown
             session={{
               user: session.data.user,
@@ -28,7 +26,7 @@ export const Navbar = () => {
               <Button>Sign Up</Button>
             </Link>
           </>
-        )}
+        )} */}
       </nav>
     </header>
     // <header className="w-full border-b">
--- src/modules/auth/templates/auth-page.tsx
+++ src/modules/auth/templates/auth-page.tsx
@@ -1,22 +1,11 @@
 'use client';
-import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/navigation';
+// import { useSession } from 'next-auth/react';
 
 import AuthForm from '../components/auth-form';
 
 export default function AuthPageTemplate() {
-  const session = useSession();
-
-  const router = useRouter();
-
-  useEffect(() => {
-    if (session.status === 'authenticated') {
-      // Redirect to the home page if the user is already authenticated
-      router.push('/');
-    }
-  }, [session]);
-
   return (
     <div className="container mx-auto flex flex-col items-center py-10">
       <div className="flex w-1/2 flex-col gap-3">
